d2b441c47d40758a11edb339a68d04b2260deb7a
Truong <hankimtruong@gmail.com>
2026-05-29 01:53:10 +0700

    file lỗi

--- src/demo_high_risk.py
+++ src/demo_high_risk.py
@@ -1,40 +1,62 @@
-def process_heavy_graph_logic(matrix_data, nodes_count, threshold_val):
-    """
-    Hàm cố tình bện chặt quá nhiều toán tử và toán hạng toán học 
-    để kích hoạt cảnh báo rủi ro lỗi logic cao (Mức 2) từ AI
-    """
-    if not matrix_data or nodes_count <= 0:
-        return []
+import math
 
-    output_matrix = []
-    
-    # Tầng lặp 1: Duyệt node
+def process_heavy_graph_logic_part1(matrix_data, nodes_count, threshold_val):
+    if not matrix_data or nodes_count <= 0: return []
+    output = []
     for i in range(nodes_count):
-        row_cells = []
-        # Tầng lặp 2: Duyệt quan hệ
+        row = []
         for j in range(nodes_count):
-            # Các ngã rẽ logic lặp lại liên tục kết hợp tính toán số mũ phình to Halstead Volume
             if i == j:
-                cell_score = (threshold_val ** 3) / (i + j + 1.0)
-                if cell_score > 50.0 and (i % 2 == 0 or j % 3 == 0):
-                    cell_score = cell_score * 0.15 + (i * j)
-                else:
-                    cell_score = 0.0
+                val = (threshold_val ** 3) / (i + j + 1.0)
+                if val > 50.0 and (i % 2 == 0 or j % 3 == 0):
+                    val = val * 0.15 + (i * j)
+                else: val = 0.0
             else:
-                # Ép các toán tử logic và toán hạng duy nhất tăng chóng mặt
-                diff_factor = abs(i - j)
-                if diff_factor > 2 and matrix_data[i][j] != 0:
-                    intermediate_calc = (matrix_data[i][j] * 2.71828) / (diff_factor * 1.414)
-                    if intermediate_calc < threshold_val:
-                        cell_score = intermediate_calc * (i ** 2)
-                    elif intermediate_calc >= threshold_val and i + j > 10:
-                        cell_score = intermediate_calc / (j ** 2)
-                    else:
-                        cell_score = intermediate_calc
+                diff = abs(i - j)
+                if diff > 2 and matrix_data[i][j] != 0:
+                    calc = (matrix_data[i][j] * 2.71828) / (diff * 1.414)
+                    if calc < threshold_val: val = calc * (i ** 2)
+                    elif calc >= threshold_val and i + j > 10: val = calc / (j ** 2)
+                    else: val = calc
+                else: val = -1.0
+            row.append(float(val))
+        output.append(row)
+    return output
+
+def process_heavy_graph_logic_part2(matrix_data, nodes_count, threshold_val):
+    # Sao chép và tăng độ phức tạp bằng cách đổi biến liên tục
+    if nodes_count < 5: return []
+    res = []
+    for x in range(nodes_count):
+        tmp = []
+        for y in range(nodes_count):
+            if x != y and matrix_data[x][y] > 0:
+                a1 = math.sin(matrix_data[x][y]) * threshold_val
+                a2 = math.cos(threshold_val) * x
+                if a1 > a2 or (a1 + a2) < 0:
+                    for k in range(10):
+                        a1 += (k * 0.1)
+                    tmp.append(a1 * a2)
                 else:
-                    cell_score = -1.0
-                    
-            row_cells.append(float(cell_score))
-        output_matrix.append(row_cells)
-        
-    return output_matrix
+                    tmp.append(0.0)
+            else:
+                tmp.append(-99.0)
+        res.append(tmp)
+    return res
+
+def master_decision_module(matrix_data, nodes_count, threshold_val):
+    # Hàm thứ 3 bện chặt kết quả của 2 hàm trên để đẩy chỉ số lên đỉnh điểm
+    data1 = process_heavy_graph_logic_part1(matrix_data, nodes_count, threshold_val)
+    data2 = process_heavy_graph_logic_part2(matrix_data, nodes_count, threshold_val)
+    
+    final_score = 0.0
+    if len(data1) == len(data2) and len(data1) > 0:
+        for r in range(len(data1)):
+            for c in range(len(data1[0])):
+                if data1[r][c] == -1.0 or data2[r][c] == -99.0:
+                    final_score += (threshold_val * 1.5)
+                else:
+                    final_score -= (data1[r][c] / (data2[r][c] + 0.001))
+                    if final_score > 1000:
+                        final_score = math.sqrt(final_score)
+    return final_score

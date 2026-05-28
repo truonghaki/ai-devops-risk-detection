7afd955376d6c5f915203ea1f66e344aafdda201
Truong <hankimtruong@gmail.com>
2026-05-29 01:57:34 +0700

    lỗi

--- src/demo_high_risk.py
+++ src/demo_high_risk.py
@@ -1,62 +1,77 @@
 import math
 
-def process_heavy_graph_logic_part1(matrix_data, nodes_count, threshold_val):
-    if not matrix_data or nodes_count <= 0: return []
-    output = []
+def catastrophic_spaghetti_monster_function(matrix_data, nodes_count, threshold_val, mode_flag, retry_limit):
+    """
+    HÀM QUÁI VẬT: Cố tình phá vỡ mọi quy chuẩn lập trình sạch.
+    Lồng ghép 5 tầng vòng lặp và if-else vào một hàm duy nhất để ép AI chấm Mức 2.
+    """
+    if not matrix_data or nodes_count <= 0 or threshold_val == 0:
+        return -1.0
+
+    global_score = 0.0
+    output_mega_array = []
+
+    # TẦNG 1: Vòng lặp chính
     for i in range(nodes_count):
-        row = []
+        sub_layer_1 = []
+        # TẦNG 2: Vòng lặp lồng 1
         for j in range(nodes_count):
             if i == j:
-                val = (threshold_val ** 3) / (i + j + 1.0)
-                if val > 50.0 and (i % 2 == 0 or j % 3 == 0):
-                    val = val * 0.15 + (i * j)
-                else: val = 0.0
+                # TẦNG 3: Rẽ nhánh sâu
+                val = (threshold_val ** 4) / (i + j + 1.0)
+                if val > 25.0:
+                    # TẦNG 4: Rẽ nhánh sâu hơn nữa
+                    for k in range(int(nodes_count)):
+                        if k % 2 == 0 and mode_flag == "advanced":
+                            val += (k * float(matrix_data[0][0]))
+                            if val > 1000.0:
+                                val = math.log(val)
+                        else:
+                            val -= (k * 0.5)
+                else:
+                    val = 0.0
             else:
                 diff = abs(i - j)
-                if diff > 2 and matrix_data[i][j] != 0:
-                    calc = (matrix_data[i][j] * 2.71828) / (diff * 1.414)
-                    if calc < threshold_val: val = calc * (i ** 2)
-                    elif calc >= threshold_val and i + j > 10: val = calc / (j ** 2)
-                    else: val = calc
-                else: val = -1.0
-            row.append(float(val))
-        output.append(row)
-    return output
-
-def process_heavy_graph_logic_part2(matrix_data, nodes_count, threshold_val):
-    # Sao chép và tăng độ phức tạp bằng cách đổi biến liên tục
-    if nodes_count < 5: return []
-    res = []
-    for x in range(nodes_count):
-        tmp = []
-        for y in range(nodes_count):
-            if x != y and matrix_data[x][y] > 0:
-                a1 = math.sin(matrix_data[x][y]) * threshold_val
-                a2 = math.cos(threshold_val) * x
-                if a1 > a2 or (a1 + a2) < 0:
-                    for k in range(10):
-                        a1 += (k * 0.1)
-                    tmp.append(a1 * a2)
+                if diff > 1:
+                    # TẦNG 3: Ngã rẽ nhánh khác
+                    if matrix_data[i][j] != 0:
+                        calc = (matrix_data[i][j] * 2.71828) / (diff * 1.414)
+                        # TẦNG 4: Lồng tiếp
+                        if calc < threshold_val:
+                            for x in range(10):
+                                if x > retry_limit:
+                                    val = calc * (i ** 2) + x
+                                else:
+                                    val = calc - x
+                        elif calc >= threshold_val and i + j > 5:
+                            val = calc / (j ** 2)
+                        else:
+                            val = calc
                 else:
-                    tmp.append(0.0)
-            else:
-                tmp.append(-99.0)
-        res.append(tmp)
-    return res
+                    # TẦNG 3 phụ
+                    if mode_flag == "debug":
+                        val = -999.0
+                    else:
+                        val = -1.0
+            
+            sub_layer_1.append(val)
+        output_mega_array.append(sub_layer_1)
 
-def master_decision_module(matrix_data, nodes_count, threshold_val):
-    # Hàm thứ 3 bện chặt kết quả của 2 hàm trên để đẩy chỉ số lên đỉnh điểm
-    data1 = process_heavy_graph_logic_part1(matrix_data, nodes_count, threshold_val)
-    data2 = process_heavy_graph_logic_part2(matrix_data, nodes_count, threshold_val)
-    
-    final_score = 0.0
-    if len(data1) == len(data2) and len(data1) > 0:
-        for r in range(len(data1)):
-            for c in range(len(data1[0])):
-                if data1[r][c] == -1.0 or data2[r][c] == -99.0:
-                    final_score += (threshold_val * 1.5)
+    # Đoạn tính toán Halstead phình to đỉnh điểm, liên tục dùng toán tử và toán hạng độc nhất
+    total_elements = len(output_mega_array) * len(output_mega_array[0]) if output_mega_array else 0
+    if total_elements > 0 and mode_flag != "safe":
+        for r in range(len(output_mega_array)):
+            for c in range(len(output_mega_array[0])):
+                # Tầng rẽ nhánh cuối cùng ép bùng nổ Effort
+                if output_mega_array[r][c] == -999.0:
+                    global_score += (threshold_val * 2.5)
+                elif output_mega_array[r][c] == -1.0:
+                    global_score += (threshold_val * 0.5)
                 else:
-                    final_score -= (data1[r][c] / (data2[r][c] + 0.001))
-                    if final_score > 1000:
-                        final_score = math.sqrt(final_score)
-    return final_score
+                    global_score -= (output_mega_array[r][c] / (threshold_val + 0.0001))
+                    if global_score > 5000.0:
+                        global_score = math.sqrt(global_score)
+                        if global_score > 100.0:
+                            global_score = global_score ** 1.5
+
+    return global_score
